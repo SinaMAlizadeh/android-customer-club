e39812115da5ea9ec3162510aff052b6f6a32a7b
sina alizadeh <32089050+SinaMAlizadeh@users.noreply.github.com>
2024-12-21 13:46:11 +0330

    add: icon

--- services/auth/index.ts
+++ services/auth/index.ts
@@ -3,16 +3,13 @@ import {loginPayload, verifyOtpPayload} from './type';
 import {ContentType} from '../../type';
 
 const login = async (payload: loginPayload) =>
-  await axios.post(
-    'http://130.185.78.214:5000/gateway/auth/Authenticate/SignInOtp',
-    {
-      phoneNumber: payload?.phoneNumber,
-    },
-  );
+  await axios.post('https://api.padix.ir/gateway/auth/Authenticate/SignInOtp', {
+    phoneNumber: payload?.phoneNumber,
+  });
 
 const checkOtp = async (payload: verifyOtpPayload) => {
   const response = await axios.post(
-    'http://130.185.78.214:5000/gateway/auth/Authenticate/VerifyOtp',
+    'https://api.padix.ir/gateway/auth/Authenticate/VerifyOtp',
     payload,
     {
       headers: {
--- services/fcm/index.ts
+++ services/fcm/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {UpdateInfoPayload} from './type';
 
 const updateInfo = async (payload: UpdateInfoPayload) =>
-  await axios.post('http://130.185.78.214:5000/gateway/auth/AppInfo', payload);
+  await axios.post('https://api.padix.ir/gateway/auth/AppInfo', payload);
 
 const infoService = {
   updateInfo,
